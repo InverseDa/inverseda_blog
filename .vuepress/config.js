9cb51f78e8570acd007dff852f947e62024d94fa
缪克达 <inverse_boom@outlook.com>
2022-08-09 20:21:44 +0800

    Update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,101 +1,111 @@
 module.exports = {
-  "title": "InverseDa's Blog",
-  "description": "None",
-  "dest": "public",
-  "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
-    ],
-    [
-      "meta",
-      {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
-  ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
-      {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
-      },
-      {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          }
+    "title": "InverseDa's Blog",
+    "description": "None",
+    "dest": "public",
+    "head": [
+        [
+            "link",
+            {
+                "rel": "icon",
+                "href": "/favicon.ico"
+            }
+        ],
+        ['link', {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css'}],
+        ['link', {
+            rel: "stylesheet",
+            href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css"
+        }],
+        [
+            "meta",
+            {
+                "name": "viewport",
+                "content": "width=device-width,initial-scale=1,user-scalable=no"
+            }
         ]
-      },
-      {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "GitHub",
-            "link": "https://github.com/InverseDa",
-            "icon": "reco-github"
-          }
-        ]
-      }
     ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
+    "theme": "reco",
+    "themeConfig": {
+        "subSidebar": 'auto',
+        "nav": [
+            {
+                "text": "Home",
+                "link": "/",
+                "icon": "reco-home"
+            },
+            {
+                "text": "TimeLine",
+                "link": "/timeline/",
+                "icon": "reco-date"
+            },
+            {
+                "text": "Docs",
+                "icon": "reco-message",
+                "items": [
+                    {
+                        "text": "vuepress-reco",
+                        "link": "/docs/theme-reco/"
+                    }
+                ]
+            },
+            {
+                "text": "Contact",
+                "icon": "reco-message",
+                "items": [
+                    {
+                        "text": "GitHub",
+                        "link": "https://github.com/InverseDa",
+                        "icon": "reco-github"
+                    }
+                ]
+            }
+        ],
+        "sidebar": {
+            "/docs/theme-reco/": [
+                "",
+                "theme",
+                "plugin",
+                "api"
+            ]
+        },
+        "type": "blog",
+        "blogConfig": {
+            "category": {
+                "location": 2,
+                "text": "Category"
+            },
+            "tag": {
+                "location": 3,
+                "text": "Tag"
+            }
+        },
+        "friendLink": [
+            {
+                "title": "午后南杂",
+                "desc": "Enjoy when you can, and endure when you must.",
+                "email": "1156743527@qq.com",
+                "link": "https://www.recoluan.com"
+            },
+            {
+                "title": "vuepress-theme-reco",
+                "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+                "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+                "link": "https://vuepress-theme-reco.recoluan.com"
+            }
+        ],
+        "logo": "/logo.png",
+        "search": true,
+        "searchMaxSuggestions": 10,
+        "lastUpdated": "Last Updated",
+        "author": "InverseDa",
+        "authorAvatar": "/avatar.jpg",
+        "record": "xxxx",
+        "startYear": "2022"
     },
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
-      },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
+    "markdown": {
+        "lineNumbers": true
     },
-    "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
-    ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "InverseDa",
-    "authorAvatar": "/avatar.jpg",
-    "record": "xxxx",
-    "startYear": "2022"
-  },
-  "markdown": {
-    "lineNumbers": true
-  }
+    extendMarkdown(md) {
+        md.set({html: true});
+        md.use(require("markdown-it-katex"));
+    },
 }
