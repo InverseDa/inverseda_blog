bc16e4a1b74628492fbd418c737bf6c118b71a03
缪克达 <inverse_boom@outlook.com>
2022-08-09 22:27:37 +0800

    Update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -38,16 +38,6 @@ module.exports = {
                 "icon": "reco-date"
             },
             {
-                "text": "Docs",
-                "icon": "reco-message",
-                "items": [
-                    {
-                        "text": "vuepress-reco",
-                        "link": "/docs/theme-reco/"
-                    }
-                ]
-            },
-            {
                 "text": "Contact",
                 "icon": "reco-message",
                 "items": [
@@ -78,20 +68,6 @@ module.exports = {
                 "text": "Tag"
             }
         },
-        "friendLink": [
-            {
-                "title": "午后南杂",
-                "desc": "Enjoy when you can, and endure when you must.",
-                "email": "1156743527@qq.com",
-                "link": "https://www.recoluan.com"
-            },
-            {
-                "title": "vuepress-theme-reco",
-                "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-                "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-                "link": "https://vuepress-theme-reco.recoluan.com"
-            }
-        ],
         "logo": "/logo.png",
         "search": true,
         "searchMaxSuggestions": 10,
